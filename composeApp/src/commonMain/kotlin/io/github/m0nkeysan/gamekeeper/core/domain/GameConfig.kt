56a8ad4b2eeacd4c48553ea632ec84b6847f39df
Thibaut <thibaut.farcin@gmail.com>
2026-01-13 09:33:18 +0100

    remove deprecated const

--- composeApp/src/commonMain/kotlin/io/github/m0nkeysan/gamekeeper/core/domain/GameConfig.kt
+++ composeApp/src/commonMain/kotlin/io/github/m0nkeysan/gamekeeper/core/domain/GameConfig.kt
@@ -6,14 +6,4 @@ object GameConfig {
 
     const val TAROT_MIN_PLAYERS = 3
     const val TAROT_MAX_PLAYERS = 5
-    
-    // Deprecated aliases for backward compatibility
-    @Deprecated("Use YAHTZEE_MIN_PLAYERS", replaceWith = ReplaceWith("YAHTZEE_MIN_PLAYERS"))
-    const val yahtzeeMinPlayers = YAHTZEE_MIN_PLAYERS
-    @Deprecated("Use YAHTZEE_MAX_PLAYERS", replaceWith = ReplaceWith("YAHTZEE_MAX_PLAYERS"))
-    const val yahtzeeMaxPlayers = YAHTZEE_MAX_PLAYERS
-    @Deprecated("Use TAROT_MIN_PLAYERS", replaceWith = ReplaceWith("TAROT_MIN_PLAYERS"))
-    const val tarotMinPlayers = TAROT_MIN_PLAYERS
-    @Deprecated("Use TAROT_MAX_PLAYERS", replaceWith = ReplaceWith("TAROT_MAX_PLAYERS"))
-    const val tarotMaxPlayers = TAROT_MAX_PLAYERS
 }
